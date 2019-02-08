6fa88d61bed890af06a0230ec73f2481b825f115
sdll <shura.sdll@gmail.com>
2019-02-09 00:44:15 +0300

    tweak the architecture

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pipenv run bash -c "PYTHONPATH=. python -m lingofunk_classify_sentiment.model.hnatt.run Restaurants 100000 glove-840B-300d"
+pipenv run bash -c "PYTHONPATH=. python -m lingofunk_classify_sentiment.model.hnatt.run Restaurants 1000000 glove-840B-300d"
